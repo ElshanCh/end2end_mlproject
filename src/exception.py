4887cfb87cd4391473cdbff655556183e3f46bd6
ElshanCh <elshan.chelebi@hotmail.com>
2024-02-12 01:14:45 +0100

    Data Transformation addes

--- src/exception.py
+++ src/exception.py
@@ -26,5 +26,6 @@ if __name__ == "__main__":
     try:
         a=1/0
     except Exception as e:
-        logging.info("Divide by Zero")
+        # logging.info("Divide by Zero")
+        logging.info(f"ERROR: {str(e)}")
         raise CustomException(e,sys)
